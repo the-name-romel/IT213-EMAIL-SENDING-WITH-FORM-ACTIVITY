af7bd70977fabfb7367115b758dd17f34144512d
the-name-romel <rommelbon100@gmail.com>
2021-10-30 14:27:26 +0800

    Validating fields

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // Variables
-const sendBtn = document.getElementById('sendBtn');
-
+const sendBtn = document.getElementById('sendBtn'),
+      email = document.getElementById('email'),
+      subject = document.getElementById('subject'),
+      message = document.getElementById('message');
 
 
 //Even Listeners
@@ -10,6 +12,12 @@ eventListeners();
 function eventListeners() {
     // app init
     document.addEventListener('DOMContentLoaded', appInit);
+
+    //validate the forms
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
+
 }
 
 
@@ -20,3 +28,39 @@ function appInit() {
     // disable the send button on load
     sendBtn.disabled = true;
 } 
+
+//validate the fields
+function validateField () {
+    let errors;
+
+    //validate the length of the field
+    validateLength(this)
+
+    //validate the email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+}
+//validate the length of the fields
+function validateLength(field){
+    if(field.value.length > 0){
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+//validate email (checks for @ in the value)
+
+function validateEmail(field) {
+    let emailText = field.value;
+    //check if the emailText contains the @ sign
+    if(emailText.indexOf('@') !== -1) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }  
+}
